7720fd028a87d09528f18c42071c2aa8039ad751
PWNEDJonesy <PWNEDJonesy@users.noreply.github.com>
2021-10-06 01:37:47 +0500

    commit to master

--- main.py
+++ main.py
@@ -21,8 +21,6 @@ def paginate(request, selection):
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
-  migrate = Migrate(app, db)
-  db.init_app(app)
   CORS(app)
 
   @app.route('/airplanes', methods=["GET"])
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from sqlalchemy.sql.elements import collate
 from sqlalchemy.sql.expression import column
 
@@ -14,6 +15,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    migrate = Migrate(app, db)
     db.create_all()
 
 
