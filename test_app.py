0b67511c82726eeb0c641bc1152404eec5cb0182
PWNEDJonesy <PWNEDJonesy@users.noreply.github.com>
2021-10-06 00:19:48 +0500

    commit to master

--- test_app.py
+++ test_app.py
@@ -34,83 +34,23 @@ class TriviaTestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertTrue(data["categories"])
-
-    def test_get_categories_wrong_method(self):
-        res = self.client().post("/categories")
-
-        self.assertEqual(res.status_code, 405)
-
-    def test_get_questions(self):
-        res = self.client().get("/questions")
+#################################################################
+    def test_get_airplanes(self):
+        res = self.client().get("/airplanes")
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        self.assertTrue(data["questions"])
-        self.assertTrue(data["total_questions"] >= 1)
-        self.assertTrue(data["current_category"])
-        self.assertTrue(data["categories"])
-
-    def test_get_questions_wrong_method(self):
-        res = self.client().get("/questions")
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 405)
-
-    def test_delete_question(self):
-        res = self.client().delete("/questions/1")
-
-        self.assertEqual(res.status_code, 200)
-
-    def test_delete_question_out_of_range(self):
-        res = self.client().delete("/questions/1000")
-
-        self.assertEqual(res.status_code, 404)
-
-    def test_create_question(self):
-        res = self.client().post("/questions", {
-                                                "question": 'Test',
-                                                "answer": "Test",
-                                                "category": "Test",
-                                                "difficulty": "Test"
-                                                }
-                                 )
+        self.assertTrue(data["airplanes"])
+    
+    def test_get_airports(self):
+        res = self.client().get("/airports")
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
-        self.assertTrue(data["questions"])
-        self.assertTrue(data["total_questions"] >= 1)
-        self.assertTrue(data["current_category"])
+        self.assertTrue(data["airports"])
 
-    def test_create_question_wrong_method(self):
-        res = self.client().delete("/questions", {
-                                                "question": 'Test',
-                                                "answer": "Test",
-                                                "category": "Test",
-                                                "difficulty": "Test"
-                                                }
-                                   )
-
-        self.assertEqual(res.status_code, 422)
-
-    def test_get_questions_by_category(self):
-        res = self.client().get("/category/1/questions")
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data["questions"])
-        self.assertTrue(data["total_questions"] >= 1)
-        self.assertTrue(data["current_category"])
-
-    def test_play_game(self):
-        res = self.client().post("/quizzes", {
-                                            "category": "science"
-                                            }
-                                 )
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertTrue(data["question"])
-        self.assertTrue(data["current_category"])
+    def test_post_airplanes(self):
+        res = self.client().post()
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
